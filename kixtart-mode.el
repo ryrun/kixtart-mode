c7e643291cce87785cdc9c27d01dbfeb19c33dfd
ryrun <ryrun@online.de>
2015-06-09 18:50:16 +0200

    version number change

--- kixtart-mode.el
+++ kixtart-mode.el
@@ -2,7 +2,7 @@
 
 ;; Author:  Ryrun <https://github.com/ryrun>
 ;; Keywords: languages
-;; Version: 20150608.2
+;; Version: 20150609.3
 ;; Homepage: https://github.com/ryrun/kixtart-mode
 ;; Package-Requires: ((emacs "24"))
 
